5812ded5cf2bf048ec2a273a6b5ba8798d487cb7
1192825 <v.i.s.h.w.a.08.11.2001@gmail.com>
2023-08-30 13:36:11 +0530

    feat: (Vishwa R) add auth guards and auth service files
    - modify: app.routing.module.ts file to custom route /dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [{
-  path: '',component: DashboardComponent
-}];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'dashboard', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
