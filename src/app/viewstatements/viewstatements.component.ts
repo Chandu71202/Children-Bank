f4061fbb7d6e902ccd9e1823720b6d3f595a6c5a
ESWAR CHAND <chandu71202@gmail.com>
2023-08-31 17:36:11 +0530

    fix:(Eswar) View Statements display

--- src/app/viewstatements/viewstatements.component.ts
+++ src/app/viewstatements/viewstatements.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-viewstatements',
   templateUrl: './viewstatements.component.html',
-  styleUrls: ['./viewstatements.component.css'],
+  styleUrls: ['./viewstatements.component.css']
 })
-export class ViewstatementsComponent {
-  constructor(public dialogRef: MatDialogRef<ViewstatementsComponent>) {}
+export class ViewstatementsComponent implements OnInit{
+  account_data :any;
+  user_id = sessionStorage.getItem('id');
+  constructor(public dialogRef: MatDialogRef<ViewstatementsComponent>,private accountservice:AccountService) {}
+  ngOnInit(): void {
+    let resp = this.accountservice.getAccount(this.user_id);
+    resp.subscribe((data) => {this.account_data = data;console.log(this.account_data)});
+  }
 
   viewStatements(): void {
-    this.dialogRef.close();
+      this.dialogRef.close();
   }
 }
