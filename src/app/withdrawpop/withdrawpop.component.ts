79e9c2cf5b635499c7a6ee8b973b706592a84f8c
ESWAR CHAND <chandu71202@gmail.com>
2023-08-31 15:33:46 +0530

    feat:(Eswar) Added Backend Functionalities to Withdraw,Depoist,Transfer feilds

--- src/app/withdrawpop/withdrawpop.component.ts
+++ src/app/withdrawpop/withdrawpop.component.ts
@@ -4,6 +4,8 @@ import {
   MatDialogModule,
   MAT_DIALOG_DATA,
 } from '@angular/material/dialog';
+import { AccountService } from '../account.service';
+import { DashboardComponent } from '../dashboard/dashboard.component';
 
 export interface ModalData {
   name: string;
@@ -15,16 +17,32 @@ export interface ModalData {
   templateUrl: './withdrawpop.component.html',
   styleUrls: ['./withdrawpop.component.css']
 })
-export class WithdrawpopComponent implements OnInit{
-  constructor(
-    public dialogRef: MatDialogRef<WithdrawpopComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: ModalData
-  ) {}
-  onNoClick(): void {
+export class WithdrawpopComponent implements OnInit {
+  withdrawAmount!: number;
+  res: any;
+  account: any;
+  user_id = sessionStorage.getItem('id');
+  result: any;
+
+  constructor(public dialogRef: MatDialogRef<WithdrawpopComponent>, private accountservice: AccountService) { }
+  ngOnInit() { }
+
+  withdraw(): void {
+    this.accountservice.getAccount(this.user_id).subscribe((res) => {
+      this.account = res;
+      if (this.account.balance < this.withdrawAmount) {
+        alert(`Insufficient Balance, your current balance is ${this.account.balance}`);
+        return;
+      }
+      const newBalance = this.account.balance - this.withdrawAmount;
+      this.accountservice.updateBalance(this.user_id, newBalance)
+        .subscribe(response => {
+          this.result = response;
+          alert(`Withdraw Successful, you current balance is ${this.result.balance}`);
+        });
+    });
     this.dialogRef.close();
   }
-  ngOnInit(){
-    
-  }
+
 
 }
